74bf433536a734d5eee3ec6895854072af6e9f93
Georgia Tardy <georgiatardyart@gmail.com>
2017-08-13 15:58:08 -0400

    first commit, added packages and boilerplate code

---
+++ main.js
@@ -0,0 +1,83 @@
+//===Packages===//
+const express = require('express');
+const bcryptjs = require('bcryptjs');
+const bodyParser = require('body-parser');
+const expressFlash = require('express-flash-messages');
+const handlebars = require('express-handlebars');
+const session = require('express-session');
+const mongodb = require('mongodb');
+const passport = require('passport');
+
+
+//===Require Mongoose and Bluebird===//
+const mongoose = require('mongoose');
+const bluebird = require('bluebird');
+
+
+//===Models for users and code snippets===//
+const User = require('./models/users');
+const codeSnippet = require('./models/codeSnippet');
+
+
+//===Tell Express to use Handlebars===//
+app.engine('handlebars', handlebars());
+app.set('views', './views');
+app.set('view engine', 'handlebars');
+
+
+//===Create Session===//
+app.use(
+  session({
+    secret: 'hush hush',
+    resave: false,
+    saveUninitialized: true
+  })
+);
+
+
+//===Connect Passport to Express boilerplate===//
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+
+//===BodyParser Middleware===//
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+
+//===Middleware that checks if user in in session, if not redirect to login===//
+const requireLogin = (request, response, next) => {
+  if (request.user) {
+    next();
+  } else {
+    response.redirect('/login');
+  }
+};
+
+// app.get('/', requireLogin, (req, res) => {
+//       console.log(session);
+//       codeSnippet.find({
+//           createdBy: request.user.username
+//         })
+//         .then((snippets) => {
+//           res.render('home', {
+//             user: req.user.snippets:snippets
+//           })
+//         });
+
+      //===Log Out===//
+      app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+      });
+
+
+      //===Connect to Mongo via Mongoose===//
+      Mongoose
+        .connect('mongodb://localhost:27017/bcryptExample', {
+          useMongoClient: true
+        })
+        .then(() => app.listen(3000, () => console.log('App is running!')));
